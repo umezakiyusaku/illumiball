9797900264c32bea2e36d0015e5bf39faa24c5cf
hogehoge <hogehoge@gmail.com>
2022-01-27 14:08:00 +0900

    illumiballclose

---
+++ Assets/Scripts/GameClearDetector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClearDetector : MonoBehaviour
+{
+    public GameObject holeRed;
+    public GameObject holeBlue;
+    public GameObject holeGreen;
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        //すべてのボールが入ったらラベルを表示
+        if(holeRed.GetComponent<Hole>()&&holeBlue.GetComponent<Hole>()&&holeGreen.GetComponent<Hole>())
+        {
+            GUI.Label(new Rect(50, 50, 100, 30), "Game Clear!");
+        }
+    }
+}
---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    //どのボールを吸い寄せるかをタグで指定
+    public string targetTag;
+    bool isHolding;
+
+    //ボールが入っているかを返す
+    public bool IsHolding()
+    {
+        return isHolding;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == targetTag)
+        {
+            isHolding = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == targetTag)
+        {
+            isHolding = false;
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        //コライダを触れているオブジェクトのrigidbodyコンポーネントを取得
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+　　　　
+        //ボールがどの方向にあるかを計算
+        Vector3 direction = other.gameObject.transform.position - transform.position;
+        direction.Normalize();
+
+        //タグに応じてボールに力を加える
+        if (other.gameObject.tag == targetTag)
+        {
+            r.velocity *= 0.9f;
+            r.AddForce(direction * -20.0f, ForceMode.Acceleration);
+        }
+        else
+        {
+            r.AddForce(direction * 80.0f, ForceMode.Impulse);
+        }
+
+    }
+}
